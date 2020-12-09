609ed5dd110325f62e735ae1a895edda1130fd17
诺墨 <normal@normalcoder.com>
2020-12-09 15:55:51 +0800

    feat(project): init SpringBoot project(maven build)

---
+++ src/main/java/com/gitee/giteego/springboot/mavendemo/MavenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gitee.giteego.springboot.mavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenDemoApplication.class, args);
+    }
+
+}
